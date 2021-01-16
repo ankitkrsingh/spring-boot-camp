c7dede23a55bb041ad0b264e44cf328a5ddc483d
ankitkrsingh <ankitkrsingh2012@gmail.com>
2021-01-16 15:19:55 +0100

    initial commit

---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+insert into employee(name, city, hobby) values ('Ankit Kumar Singh', 'Nürnberg', 'Cricket');
+insert into employee(name, city, hobby) values ('Aron Wachter', 'Nürnberg', 'Frisbee');
+insert into employee(name, city, hobby) values ('Lukas Dallhammer', 'Nürnberg', 'Football');
+insert into employee(name, city, hobby) values ('Max Friedrich', 'Lauf', 'Reading');
+insert into employee(name, city, hobby) values ('Sami Alzein', 'Nürnberg', 'Football');
